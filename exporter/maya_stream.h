64e969713a88ee12a9fdbf3dc3c4385f7f36a76f
Martin Prazak <martin.prazak@gmail.com>
2012-04-20 16:40:13 -0700

    maya_stream - code cleanup

--- exporter/maya_stream.h
+++ exporter/maya_stream.h
@@ -3,20 +3,12 @@
 #include <sstream>
 #include <maya/MFnPlugin.h>
 
-// uber-simple way of supporting endl (any other manipulator would end up behaving the same as endl, but who cares in this super-simple class)
-typedef std::basic_ostream<char, std::char_traits<char> > cout_type;
-typedef cout_type& (*endl_type)(cout_type&);
-
 class maya_stream {
 public:
 	template<typename T>
 	maya_stream& operator << (const T& val);
 
 	maya_stream& operator<<(std::ostream&(*f)(std::ostream&));
-
-	template<>
-	maya_stream& maya_stream::operator << (const endl_type& val);
-
 
 protected:
 private:
@@ -31,6 +23,7 @@ maya_stream& maya_stream::operator << (const T& val) {
 	return *this;
 };
 
+// uber-simple way of supporting endl
 maya_stream& maya_stream::operator<<(std::ostream&(*f)(std::ostream&)) {
     if( f == std::endl ) {
         MGlobal::displayInfo( m_stream.str().c_str() );
